d6d2423582ed3f4fda77f607b734a26e24690c66
Omar Farhat <oibfarhat@gmail.com>
2019-07-17 13:42:53 -0400

    Uncommitted changes

--- bin/setup_cluster.sh
+++ bin/setup_cluster.sh
@@ -8,6 +8,7 @@ bin=`cd "$bin"; pwd`
 
 init_zk_multinodes_conf(){
     # zk multi-nodes
+    echo "Setting up ZK confs"
     if [[ $HAS_HOSTS ]]; then
 	local counter=0
 	while read line
@@ -30,6 +31,23 @@ init_zk_multinodes_conf(){
     fi
 }
 
+init_kafka_multinodes_conf(){
+    # zk multi-nodes
+    echo "Setting up KAFKA confs"
+    if [[ $HAS_HOSTS ]]; then
+	local counter=0
+	while read line
+	do
+            ((counter++))
+            echo "SSH-ing to $line..."
+            ssh ${line} "
+                cp $HOME/klink-benchmarks/benchmark/kafka/config/server.properties /tmp/data/server.properties
+                sed -i "s/broker.id=.*/broker.id=$counter/g" /tmp/data/server.properties
+		"</dev/null
+            echo "Changed broker.id=$counter to $line, leaving..."
+        done <${HOSTS_FILE}
+    fi
+}
 setup(){
     init_zk_multinodes_conf
 }
