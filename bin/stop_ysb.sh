90572d15ba59012607f145569566618bf3fa514d
Omar Farhat <oibfarhat@gmail.com>
2019-07-18 17:21:41 -0400

    Building up Flink libraries on all node

--- bin/stop_ysb.sh
+++ bin/stop_ysb.sh
@@ -89,13 +89,14 @@ stop_flink(){
 }
 
 stop_flink_processing_local() {
-    local flink_id=`"$FLINK_DIR/bin/flink" list | grep 'Flink Streaming Job' | awk '{print $4}'; true`
-    if [[ "$flink_id" == "" ]];
+    local flink_id=`\"$FLINK_DIR/bin/flink\" list | grep 'Flink Streaming Job' | awk '{print $4}'; true`
+    echo $flink_id
+    if [[ \"$flink_id\" == \"\" ]];
     then
-        echo "Could not find streaming job to kill"
+        echo \"Could not find streaming job to kill\"
     else
-        "$FLINK_DIR/bin/flink" cancel $flink_id
-         sleep 3
+        $FLINK_DIR/bin/flink cancel $flink_id
+        sleep 3
     fi
 }
 
