bf6c9aee9c4f1ab661d53a7d147b43fe5035756f
Omar Farhat <oibfarhat@gmail.com>
2019-12-16 13:35:59 -0500

    Adding support for all repos

--- bin/gen-exp.sh
+++ bin/gen-exp.sh
@@ -21,16 +21,17 @@ gen_exp(){
     # experiment file
     echo 'experiment_name:' $1 > $exp_file
     echo 'workload_type:' $2 >> $exp_file
-    echo 'policy_index:' $3 >> $exp_file
-    echo 'num_instances:' $4 >> $exp_file
-    echo 'throughput:' $5 >> $exp_file
-    echo 'watermark_frequency:' $6 >> $exp_file
-    echo 'window_size:' $7 >> $exp_file
+    echo 'num_instances:' $3 >> $exp_file
+    echo 'throughput:' $4 >> $exp_file
+    echo 'watermark_frequency:' $5 >> $exp_file
+    echo 'window_size:' $6 >> $exp_file
+    ## TODO(oibfarhat): Find a solution for scheduling policy
+    # echo 'policy_index:' $3 >> $exp_file
 }
 
 if [[ $# -lt 7 ]];
 then
-  echo "Invalid use: gen-exp.sh <experiment_name> <workload_type> <policy_index> <num_instances> <throughput> <watermark_frequency> <window_size_in_seconds>"
+  echo "Invalid use: gen-exp.sh <experiment_name> <workload_type> <num_instances> <throughput> <watermark_frequency> <window_size_in_seconds>"
 else
-    gen_exp $1 $2 $3 $4 $5 $6 $7
+    gen_exp $1 $2 $3 $4 $5 $6
 fi
