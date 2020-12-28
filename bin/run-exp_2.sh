cf2135fd6110ce8ed180f7f3417dc498e7670955
ChasonPickles <harshbindra@live.ca>
2020-12-28 15:08:44 -0500

    - modify original run-exp and add run-exp_2

--- bin/run-exp_2.sh
+++ bin/run-exp_2.sh
@@ -5,33 +5,35 @@ bin=`cd "$bin"; pwd`
 
 . "$bin"/config.sh
 
-TEST_TIME=${TEST_TIME:-240}
+TEST_TIME=${TEST_TIME:-120}
 
 run_exp(){
     ## Verify experiment exists
     if [[ ! -f $EXPERIMENTS_DIR/$1.yaml ]];
 	then
 	    echo "Experiment file does not exist"
-	    exit -1
+	    exit
 	fi
 
 	# Fetch the workload type
-	local workload_type=$(yq r "$EXPERIMENTS_DIR/$1.yaml" "workload_type")
+	local workload_type="unassigned"
+	workload_type=$(yq r "$EXPERIMENTS_DIR/$1.yaml" "workload_type")
     cd $BIN_DIR
 
     if [[ "$workload_type" = "ysb" ]]; then
         echo "Running YSB Experiment"
         # Run YSB
-        ./start_ysb.sh "$EXPERIMENTS_DIR/$1.yaml"
-        echo "Sleeping for $TEST_TIME to let experiment run"
+        ./start_ysb_2.sh "$EXPERIMENTS_DIR/$1.yaml"
+        echo "Sleeping for $TEST_TIME to let experiment $1 run"
         sleep $TEST_TIME
 
         # Stop YSB
-        ./stop_ysb.sh $1 10
+        ./stop_ysb_2.sh $1 10
     elif [[ "$workload_type" = "lrb" ]]; then
         echo "Running LRB Experiment"
         # Run LRB
         ./start_lrb.sh "$EXPERIMENTS_DIR/$1.yaml"
+        echo "Sleeping for $TEST_TIME to let experiment $1 run"
         sleep $TEST_TIME
 
         # Stop LRB
@@ -40,14 +42,14 @@ run_exp(){
         echo "Unknown Workload!"
     fi
 
-    echo "$Experiment $1 is done."
+    echo "Experiment $1 is done."
     sleep 20
 }
 
 
 if [[ $# -lt 1 ]];
 then
-  echo "Invalid use: ./run-exp.sh <experiment_names>"
+  echo "Invalid use: ./run-exp.sh <experiment_name>"
 else
     while [[ $# -gt 0 ]];
     do
