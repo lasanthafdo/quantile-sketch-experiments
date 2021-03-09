ed9f7b92fa6f8790151ef56fef7adf70832f5780
ChasonPickles <harshbindra@live.ca>
2021-03-08 19:41:00 -0500

    - add nyt benchmark option to workload generators

--- workload-generator/src/main/java/WorkloadGeneratorEntryPoint.java
+++ workload-generator/src/main/java/WorkloadGeneratorEntryPoint.java
@@ -1,5 +1,6 @@
 import LRB.LRBWorkloadGenerator;
 import YSB.YSBWorkloadGenerator;
+import NYT.NYTWorkloadGenerator;
 import YSB.YSBWorkloadSetup;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
@@ -12,6 +13,7 @@ import java.util.Properties;
 
 import static LRB.LRBConstants.LRB_KAFKA_TOPIC_PREFIX;
 import static YSB.YSBConstants.KAFKA_TOPIC_PREFIX;
+import static NYT.NYTConstants.NYT_KAFKA_TOPIC_PREFIX;
 
 public class WorkloadGeneratorEntryPoint {
 
@@ -71,6 +73,8 @@ public class WorkloadGeneratorEntryPoint {
             }
         } else if (workloadType.equalsIgnoreCase("lrb")) {
             runLRBWorkloadGenerator(setupMap, benchMap);
+        }else if (workloadType.equalsIgnoreCase("nyt")) {
+            runNYTWorkloadGenerator(setupMap, benchMap);
         }
     }
 
@@ -111,6 +115,25 @@ public class WorkloadGeneratorEntryPoint {
         }
     }
 
+    private static void runNYTWorkloadGenerator(Map setupMap, Map benchMap) {
+        /* Create Kafka Producer */
+        Properties props = new Properties();
+        props.put("bootstrap.servers", Utils.getKafkaBrokers(setupMap));
+
+        Producer<byte[], byte[]> kafkaProducer =
+                new KafkaProducer<>(props,
+                        new ByteArraySerializer(),
+                        new ByteArraySerializer());
+
+
+        /* Get Benchmark properties */
+        int throughput = (Integer) benchMap.get("throughput");
+
+        new Thread(new NYTWorkloadGenerator(kafkaProducer, NYT_KAFKA_TOPIC_PREFIX, "nyt-data.csv", throughput))
+                .start();
+
+    }
+
     private static void runLRBWorkloadGenerator(Map setupMap, Map benchMap) {
         /* Create Kafka Producer */
         Properties props = new Properties();
@@ -130,16 +153,14 @@ public class WorkloadGeneratorEntryPoint {
         int throughput = (Integer) benchMap.get("throughput");
         int watermarkFrequency = (Integer) benchMap.get("watermark_frequency");
 
-        for (int i = 1; i <= numOfInstances; i++) {
-            try {
-                new Thread(
-                        new LRBWorkloadGenerator(
-                                kafkaProducer, LRB_KAFKA_TOPIC_PREFIX + "-" + i, "car.dat", throughput))
-                        .start();
-                Thread.sleep(2000);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        try {
+            new Thread(
+                    new LRBWorkloadGenerator(
+                            kafkaProducer, LRB_KAFKA_TOPIC_PREFIX + "-" + 1, "car.dat", throughput))
+                    .start();
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
--- workload-processor-flink/src/main/java/WorkloadProcessorEntryPoint.java
+++ workload-processor-flink/src/main/java/WorkloadProcessorEntryPoint.java
@@ -1,5 +1,6 @@
 import LRB.LinearRoadQuery;
 import YSB.AdvertisingQuery;
+import NYT.TaxiQuery;
 import org.apache.flink.api.java.utils.ParameterTool;
 //import org.apache.flink.streaming.runtime.tasks.scheduler.StreamTaskSchedulerPolicy;
 
@@ -23,6 +24,10 @@ public class WorkloadProcessorEntryPoint {
             createYSBInstances(setupParams, experimentMap);
         } else if (workloadType.equalsIgnoreCase("lrb")) {
             createLRBInstances(setupParams, experimentMap);
+        }else if (workloadType.equalsIgnoreCase("nyt")) {
+            createNYTInstances(setupParams, experimentMap);
+        }else{
+            System.out.println("no matching workload type found");
         }
     }
 
@@ -53,6 +58,22 @@ public class WorkloadProcessorEntryPoint {
             // Run LRB query
             LinearRoadQuery lrQuery = new LinearRoadQuery(setupParams, numQueries);
             lrQuery.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createNYTInstances(ParameterTool setupParams, Map experimentMap) {
+        try {
+            // Number of queries
+            int numQueries = ((Number) experimentMap.getOrDefault("num_instances", 1)).intValue();
+
+            // Window size
+            int windowSize = ((Integer) experimentMap.getOrDefault("window_size", 3)).intValue();
+
+            // Run YSB query
+            TaxiQuery taxiQuery = new TaxiQuery(setupParams, numQueries, windowSize);
+            taxiQuery.run();
         } catch (Exception e) {
             e.printStackTrace();
         }
