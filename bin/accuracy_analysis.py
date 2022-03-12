769965eb783c7d423028accd9312148111adb07e
Lasantha Fernando <lasantha.fdo@gmail.com>
2022-03-12 01:37:36 -0500

    Updates to plotting scripts

--- bin/accuracy_analysis.py
+++ bin/accuracy_analysis.py
@@ -25,9 +25,28 @@ def produce_bar_plot(mid_q_dict, upper_q_dict, plot_title, x_label, y_label, plo
     # plt.legend(loc="lower right")
     plt.savefig(plot_filename)
     plt.clf()
-    print("Finished generating plots.")
-    # plt.show()
+    print("Finished generating plots for " + plot_title)
+
+
+def produce_line_plot(data_df, plot_title, x_axis, x_label, y_label, plot_filename):
+    print(data_df)
+    ax = data_df.plot(x=x_axis, y=["Moments", "DDS", "UDDS", "KLL", "REQ"], style=['o-', 'v-', '^-', '|--', 'x--'])
+    if x_axis == "Kurtosis":
+        ax.set_xscale('symlog')
+        ax.set_yscale('log')
+        plt.xlim(-2, 1000000)
+        # plt.ylim(0, 0.08)
+    elif x_axis == "Data Size":
+        ax.set_xscale('log')
+
+    # plt.xticks(rotation=0)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(plot_title)
+    # plt.legend(loc="lower right")
+    plt.savefig(plot_filename)
     plt.clf()
+    print("Finished generating plots.")
 
 
 def calc_accuracy(approx_df, real_df):
@@ -63,8 +82,32 @@ if __name__ == '__main__':
     report_folder = sys.argv[1]
     datasets = ['pareto', 'uniform', 'power', 'nyt']
     ds_label_names = {'pareto': 'Pareto', 'uniform': 'Uniform', 'nyt': 'NYT', 'power': 'Power'}
-    num_windows = 5
+    num_windows = 10
+    pd.set_option('display.max_columns', 12)
+    nyt_analysis = False
 
+    if nyt_analysis:
+        nyt_file = '/home/m34ferna/flink-benchmarks/nyt-data.csv'
+        nyt_names = ['medallion', 'hack_license', 'vendor_id', 'pickup_datetime', 'payment_type', 'fare_amount',
+                     'surcharge', 'mta_tax', 'tip_amount', 'tolls_amount', 'total_amount']
+        nyt_ds = pd.read_csv(nyt_file, header=None, names=nyt_names, nrows=1000000)
+        # nyt_ds = nyt_ds[(nyt_ds['total_amount'] >= 3.5) & (nyt_ds['total_amount'] <= 12.0)]
+        nyt_ds = nyt_ds.groupby('total_amount', as_index=False).count().drop(
+            ['medallion', 'hack_license', 'vendor_id', 'pickup_datetime', 'payment_type', 'fare_amount', 'surcharge',
+             'mta_tax', 'tip_amount'], axis=1)
+        nyt_ds = nyt_ds[nyt_ds['tolls_amount'] >= 2000]
+        print(nyt_ds.tail(50))
+        # nyt_ds.hist(column='total_amount', bins=100)
+        # plt.show()
+        # plt.savefig(report_folder + '/plots/nyt_dist.png')
+        # plt.clf()
+        exit(0)
+
+    missing_pcts = []
+    ddsc_upper_diff_pcts = []
+    ddsc_mid_diff_pcts = []
+    ddsc_upper_diff = []
+    ddsc_mid_diff = []
     for dataset in datasets:
         algos = ['moments', 'dds', 'ddsc', 'kll', 'req', 'udds']
         real_names = ['window_id', 'pct_01', 'pct_05', 'pct_25', 'pct_50', 'pct_75', 'pct_90', 'pct_95', 'pct_98',
@@ -77,10 +120,16 @@ if __name__ == '__main__':
                                 'pct_99', 'query_time', 'num_retained']
         mid_q_dict = {}
         upper_q_dict = {}
+        algo_ds_acc_dict = {}
         for algo in algos:
+            print("Algorithm: " + algo)
             f_algo_ds_real = report_folder + '/' + algo + '-' + dataset + '-real-cleaned.csv'
             f_algo_ds_sketch = report_folder + '/' + algo + '-' + dataset + '-sketch-cleaned.csv'
-            algo_pareto_real = pd.read_csv(f_algo_ds_real, header=None, names=real_names)
+            algo_ds_real = pd.read_csv(f_algo_ds_real, header=None, names=real_names)
+            algo_ds_real['missing_pct'] = algo_ds_real['slack_events'] / algo_ds_real['win_size'] * 100.0
+            print(algo_ds_real['missing_pct'])
+            missing_pcts.append(np.mean(algo_ds_real[algo_ds_real['missing_pct'] > 0]['missing_pct']))
+            print(missing_pcts)
 
             if algo == 'kll' or algo == 'req':
                 algo_ds_sketch = pd.read_csv(f_algo_ds_sketch, header=None, names=sketch_names_kll_req)
@@ -90,14 +139,28 @@ if __name__ == '__main__':
             if len(algo_ds_sketch.index) > num_windows:
                 algo_ds_sketch.drop(algo_ds_sketch.tail(1).index, inplace=True)
 
-            algo_ds_accuracy, mid_q_accuracy, upper_q_accuracy = calc_accuracy(algo_ds_sketch, algo_pareto_real)
+            algo_ds_accuracy, mid_q_accuracy, upper_q_accuracy = calc_accuracy(algo_ds_sketch, algo_ds_real)
             print(algo_ds_accuracy)
             mid_q_dict[algo] = np.round(np.mean(mid_q_accuracy), 4)
             upper_q_dict[algo] = np.round(np.mean(upper_q_accuracy), 4)
+            algo_ds_acc_dict[algo] = algo_ds_accuracy.mean(axis=1)
+            print("==================================================")
 
         print(mid_q_dict)
         print(upper_q_dict)
-        plot_file_path = report_folder + '/plots/' + dataset + '.pdf'
+        ddsc_mid_diff.append(round(abs(mid_q_dict["dds"] - mid_q_dict["ddsc"]),4))
+        ddsc_upper_diff.append(round(abs(upper_q_dict["dds"] - upper_q_dict["ddsc"]), 4))
+        ddsc_upper_diff_pcts.append(round(abs(upper_q_dict["dds"] - upper_q_dict["ddsc"]) * 100.0 / upper_q_dict["dds"], 4))
+        ddsc_mid_diff_pcts.append(round(abs(mid_q_dict["dds"] - mid_q_dict["ddsc"]) * 100.0 / mid_q_dict["dds"], 4))
+        plot_file_path = report_folder + '/plots/' + dataset + '_avg_errors.png'
         produce_bar_plot(mid_q_dict, upper_q_dict, ds_label_names[dataset], 'Quantile Range', 'Avg. Relative Error',
                          plot_file_path)
+        # produce_line_plot(algo_ds_acc_dict,ds_label_names[dataset], "",'Quantile Range', 'Avg. Relative Error', plot_file_path)
         input("Press Enter to continue...")
+
+    print(missing_pcts)
+    print(np.mean(missing_pcts))
+    print(ddsc_upper_diff_pcts)
+    print(ddsc_mid_diff_pcts)
+    print(ddsc_upper_diff)
+    print(ddsc_mid_diff)
--- bin/op_time_analysis.py
+++ bin/op_time_analysis.py
@@ -2,37 +2,41 @@ import sys
 
 import matplotlib.pyplot as plt
 import pandas as pd
-from itertools import cycle
 
 
 def produce_imq_bar_plot(data_df, plot_title, x_axis, x_label, y_label, plot_filename, y_limit=0.0):
     ax = data_df.plot(x=x_axis, y=["Moments", "DDS", "UDDS", "KLL", "REQ"], kind="bar")
     if y_limit > 0:
-        for p in ax.patches:
-            if p.get_height() > y_limit:
-                ax.annotate(str(p.get_height()), (p.get_x() + 0.03, 4 * y_limit / 5), rotation=90)
-        plt.ylim(0, y_limit)
+        plt.yscale('log')
+        # for p in ax.patches:
+        #     ax.annotate(str(p.get_height()), (p.get_x() + 0.03, p.get_height() * 1.25), rotation=90)
+        plt.ylim(top=y_limit)
+
+    if x_axis == "Num Sketches":
+        plt.yscale('log')
+        plt.ylim(top=5000)
+        plt.legend(loc="upper right")
 
     if x_axis == "Quantile":
         ax.set_xticklabels(["Other quantiles", "50th quantile"])
+        plt.legend(loc="upper left")
+
     plt.xticks(rotation=0)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.title(plot_title)
-    # plt.legend(loc="lower right")
     plt.savefig(plot_filename)
     plt.clf()
     print("Finished generating plots.")
-    # plt.show()
-    plt.clf()
 
 
 def produce_imq_line_plot(data_df, plot_title, x_axis, x_label, y_label, plot_filename):
     ax = data_df.plot(x=x_axis, y=["Moments", "DDS", "UDDS", "KLL", "REQ"], style=['o-', 'v-', '^-', '|--', 'x--'])
     if x_axis == "Kurtosis":
         ax.set_xscale('symlog')
+        ax.set_yscale('symlog', linthresh=1e-3)
         plt.xlim(-2, 1000000)
-        plt.ylim(0, 0.08)
+        plt.ylim(-1e-4, 0.2)
     elif x_axis == "Data Size":
         ax.set_xscale('log')
 
@@ -41,18 +45,16 @@ def produce_imq_line_plot(data_df, plot_title, x_axis, x_label, y_label, plot_fi
     plt.ylabel(y_label)
     plt.title(plot_title)
     # plt.legend(loc="lower right")
+    # plt.show()
     plt.savefig(plot_filename)
     plt.clf()
     print("Finished generating plots.")
-    # plt.show()
-    plt.clf()
 
 
 def produce_imq_scatter_plot(data_df, plot_title, x_axis, x_label, y_label, plot_filename):
-
     size = 20
     fig, ax = plt.subplots()
-    for i, alg in enumerate(["DDS","UDDS","KLL","REQ","Moments"]):
+    for i, alg in enumerate(["DDS", "UDDS", "KLL", "REQ", "Moments"]):
         offset = 0.7 + i * 0.2
         ax.scatter(data_df[x_axis] * offset, data_df[alg], s=size, label=alg)
 
@@ -73,63 +75,80 @@ def produce_imq_scatter_plot(data_df, plot_title, x_axis, x_label, y_label, plot
     plt.savefig(plot_filename)
     plt.clf()
     print("Finished generating plots.")
-    # plt.show()
-    plt.clf()
 
 
 if __name__ == '__main__':
     report_folder = sys.argv[1]
+    pd.set_option('display.max_columns', 12)
 
-    f_query_times = report_folder + '/query_times.csv'
-    query_times_df = pd.read_csv(f_query_times)
-    print(query_times_df)
-    q_plot_file_name = report_folder + '/plots/query_times.pdf'
-    produce_imq_line_plot(query_times_df, 'Query time', "Data Size", 'Data size', 'Time (microseconds)',
-                          q_plot_file_name)
+    graphs_to_plot = ['query']
+    plot_file_ext = '.pdf'
 
-    f_insert_times = report_folder + '/insert_times.csv'
-    insert_times_df = pd.read_csv(f_insert_times)
-    insert_times_df.iloc[:, 1:] = insert_times_df.iloc[:, 1:].div(insert_times_df['Data Size'], axis=0)
-    insert_times_df['Data Size'] = insert_times_df['Data Size'] // 1000000
-    print(insert_times_df)
-    i_plot_file_name = report_folder + '/plots/insert_times.pdf'
-    produce_imq_bar_plot(insert_times_df, 'Insertion time for each data point', "Data Size", 'Data size (millions)',
-                         'Time (microseconds)', i_plot_file_name)
+    if 'query' in graphs_to_plot:
+        f_query_times = report_folder + '/query_times.csv'
+        query_times_df = pd.read_csv(f_query_times)
+        query_times_df = query_times_df.groupby('Data Size', as_index=False).mean().round(4)
+        print(query_times_df)
+        q_plot_file_name = report_folder + '/plots/query_times' + plot_file_ext
+        produce_imq_line_plot(query_times_df, 'Query time', "Data Size", 'Data size', 'Time (microseconds)',
+                              q_plot_file_name)
 
-    f_merge_times = report_folder + '/merge_times.csv'
-    merge_times_df = pd.read_csv(f_merge_times)
-    print(merge_times_df)
-    i_plot_file_name = report_folder + '/plots/merge_times.pdf'
-    produce_imq_bar_plot(merge_times_df, 'Merge time', "Num Sketches", 'Number of sketches', 'Time (microseconds)',
-                         i_plot_file_name, 25000)
+    if 'insert' in graphs_to_plot:
+        f_insert_times = report_folder + '/insert_times.csv'
+        insert_times_df = pd.read_csv(f_insert_times)
+        insert_times_df.iloc[:, 1:] = insert_times_df.iloc[:, 1:].div(insert_times_df['Data Size'], axis=0)
+        insert_times_df['Data Size'] = insert_times_df['Data Size'] // 1000000
+        print(insert_times_df)
+        i_plot_file_name = report_folder + '/plots/insert_times' + plot_file_ext
+        produce_imq_bar_plot(insert_times_df, 'Insertion time for each data point', "Data Size", 'Data size (millions)',
+                             'Time (microseconds)', i_plot_file_name)
 
-    f_kurtosis = report_folder + '/kurtosis.csv'
-    kurtosis_df = pd.read_csv(f_kurtosis)
-    print(kurtosis_df)
-    kurtosis_df.iloc[:, 3:] = kurtosis_df.iloc[:, 3:].sub(kurtosis_df['Actual'], axis=0).div(
-        kurtosis_df['Actual'], axis=0).abs().round(4)
-    kurtosis_df = kurtosis_df.sort_values(by=['Kurtosis'])
-    print(kurtosis_df)
-    k_plot_file_name = report_folder + '/plots/kurtosis.pdf'
-    produce_imq_line_plot(kurtosis_df, 'Kurtosis vs Accuracy', "Kurtosis", 'Kurtosis',
-                          'Avg. Relative Error (98th percentile)', k_plot_file_name)
+    if 'merge' in graphs_to_plot:
+        f_merge_times = report_folder + '/merge_times.csv'
+        merge_times_df = pd.read_csv(f_merge_times)
+        merge_times_df['Num Merges'] = merge_times_df['Num Sketches'] - 1
+        merge_times_df.iloc[:, 1:6] = merge_times_df.iloc[:, 1:6].div(merge_times_df['Num Merges'], axis=0).round(4)
+        merge_times_df.drop('Num Merges', axis=1, inplace=True)
+        print(merge_times_df)
+        i_plot_file_name = report_folder + '/plots/merge_times' + plot_file_ext
+        produce_imq_bar_plot(merge_times_df, 'Merge time per sketch', "Num Sketches", 'Number of sketches',
+                             'Time (microseconds)',
+                             i_plot_file_name)
 
-    f_adaptability = report_folder + '/adaptability.csv'
-    adaptability_df = pd.read_csv(f_adaptability)
-    adaptability_df.iloc[:, 2:] = adaptability_df.iloc[:, 2:].sub(adaptability_df['Actual'], axis=0).div(
-        adaptability_df['Actual'], axis=0).abs().round(4)
-    print(adaptability_df)
-    adaptability_df.loc[adaptability_df['Quantile'] != 0.5, 'Quantile'] = 0
-    adaptability_df.loc[adaptability_df['Quantile'] == 0.5, 'Quantile'] = 1
-    adaptability_df = adaptability_df.groupby('Quantile', as_index=False).mean().round(4)
-    print(adaptability_df)
-    a_plot_file_name = report_folder + '/plots/adaptability.pdf'
-    produce_imq_bar_plot(adaptability_df, 'Adaptability', "Quantile", 'Quantiles',
-                         'Avg. Relative Error', a_plot_file_name, 0.08)
+    if 'kurtosis' in graphs_to_plot:
+        f_kurtosis = report_folder + '/kurtosis.csv'
+        kurtosis_df = pd.read_csv(f_kurtosis)
+        print(kurtosis_df)
+        kurtosis_df.iloc[:, 3:] = kurtosis_df.iloc[:, 3:].sub(kurtosis_df['Actual'], axis=0).div(
+            kurtosis_df['Actual'], axis=0).abs()
+        print(kurtosis_df.round(6))
+        kurtosis_df = kurtosis_df.groupby(['Data Set', 'Kurtosis', 'Actual'], as_index=False).mean().round(6)
+        kurtosis_df = kurtosis_df.sort_values(by=['Kurtosis'])
+        print(kurtosis_df)
+        k_plot_file_name = report_folder + '/plots/kurtosis_accuracy' + plot_file_ext
+        produce_imq_line_plot(kurtosis_df, 'Kurtosis vs Accuracy', "Kurtosis", 'Kurtosis',
+                              'Avg. Relative Error (98th percentile)', k_plot_file_name)
 
-    f_query_time_tests = report_folder + '/query_time_tests.csv'
-    query_time_tests_df = pd.read_csv(f_query_time_tests)
-    print(query_time_tests_df)
-    q_plot_file_name = report_folder + '/plots/query_time_tests.png'
-    produce_imq_scatter_plot(query_time_tests_df, 'Query time', "Data Size", 'Data size', 'Time (microseconds)',
-                             q_plot_file_name)
+    if 'adaptability' in graphs_to_plot:
+        f_adaptability = report_folder + '/adaptability.csv'
+        adaptability_df = pd.read_csv(f_adaptability)
+        adaptability_df.iloc[:, 2:] = adaptability_df.iloc[:, 2:].sub(adaptability_df['Actual'], axis=0).div(
+            adaptability_df['Actual'], axis=0).abs().round(4)
+        adaptability_df = adaptability_df.drop('Actual', axis=1)
+        adaptability_df = adaptability_df.groupby(['Quantile'], as_index=False).mean().round(4)
+        print(adaptability_df)
+        adaptability_df.loc[adaptability_df['Quantile'] != 0.5, 'Quantile'] = 0
+        adaptability_df.loc[adaptability_df['Quantile'] == 0.5, 'Quantile'] = 1
+        adaptability_df = adaptability_df.groupby('Quantile', as_index=False).mean().round(4)
+        print(adaptability_df)
+        a_plot_file_name = report_folder + '/plots/adaptability_avg_errors' + plot_file_ext
+        produce_imq_bar_plot(adaptability_df, 'Adaptability', "Quantile", 'Quantiles',
+                             'Avg. Relative Error', a_plot_file_name)
+
+    if 'query_time_tests' in graphs_to_plot:
+        f_query_time_tests = report_folder + '/query_time_tests.csv'
+        query_time_tests_df = pd.read_csv(f_query_time_tests)
+        print(query_time_tests_df)
+        q_plot_file_name = report_folder + '/plots/query_time_tests' + plot_file_ext
+        produce_imq_scatter_plot(query_time_tests_df, 'Query time', "Data Size", 'Data size', 'Time (microseconds)',
+                                 q_plot_file_name)
